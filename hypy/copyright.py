c13af102a63185d653456e3e8c0903948abab067
Cory Dodt <corymercurial@spam.goonmill.org>
2009-09-23 09:04:06 -0700

    releasing 0.8.4

--- hypy/copyright.py
+++ hypy/copyright.py
@@ -6,6 +6,6 @@ GENERATED FILE -- DO NOT EDIT
 from datetime import date
 
 author = "Cory Dodt"
-releaseDate = "2009-09-19"
-releaseRevision = "f6d37217eed8"
+releaseDate = "2009-09-23"
+releaseRevision = "6454ce4fc3e5+"
 __version__ = "0.8.4"
